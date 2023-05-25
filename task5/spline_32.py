b6b67598c649f8e95d1f54a9dae71356a8cce27d
Tlmonko <ktkozlov@gmail.com>
2023-05-25 21:17:40 +0300

    feat: spline 32

---
+++ task5/spline_32.py
@@ -0,0 +1,20 @@
+import numpy as np
+from typing import List
+from spline_31 import get_spline_31
+
+
+def __get_c(y_coords: List[float], h: float, i: int):
+    return 1.5 / h * (y_coords[i + 1] - y_coords[i - 1])
+
+
+def get_spline_32(x_coords: List[float], y_coords: List[float], der_0: float, der_1: float, h: float):
+    n = len(x_coords)
+    matrix = [[1.0] + [0.0] * (n - 1)] + \
+             [[1 if abs(i - x) <= 1 else 0 for i in range(n)] for x in range(1, n - 1)] + \
+             [[0.0] * (n - 1) + [1.0]]
+
+    b = [der_0] + [__get_c(y_coords, h, i) for i in range(1, n - 1)] + [der_1]
+
+    derivative_points = np.linalg.solve(matrix, b)
+
+    return lambda t: get_spline_31(t, x_coords, y_coords, derivative_points)
--- task5/task5.py
+++ task5/task5.py
@@ -1,6 +1,7 @@
 import numpy as np
 from spline_10 import get_spline_10
 from spline_31 import get_spline_31
+from spline_32 import get_spline_32
 from common.draw_plot import draw_plot
 
 
@@ -16,14 +17,19 @@ def main():
     interval = (-2, 2)
     interpolation_points_count = 5
 
+    interval_length = interval[1] - interval[0] + 1
     x = np.linspace(*interval, num=interpolation_points_count)
     interpolation_points = (x, [my_function(i) for i in x])
     derivative_points = [my_function_derivative(i) for i in x]
 
+    spline_32 = get_spline_32(*interpolation_points, derivative_points[0], derivative_points[-1],
+                              interval_length / interpolation_points_count)
+
     draw_plot(interval, [interpolation_points, interpolation_points, interpolation_points],
               [my_function, lambda t: get_spline_10(t, *interpolation_points),
-               lambda t: get_spline_31(t, *interpolation_points, derivative_points)],
-              ['function', 'spline 10', 'spline 31'])
+               lambda t: get_spline_31(t, *interpolation_points, derivative_points),
+               spline_32],
+              ['function', 'spline 10', 'spline 31', 'spline 32'])
 
 
 if __name__ == '__main__':
